fc677522fbdc7f974bb9574c52a3e80b4271d080
subrightG9021 <gabishmas@gmail.com>
2026-05-31 10:43:56 +0300

    Add files via upload

---
+++ power_training_app/Models_dis/PowerForDisPrefrence.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Concurrent;
+using System.Configuration.Assemblies;
+using System.Dynamic;
+using System.IO.Pipes;
+using System.Security.Cryptography.X509Certificates;
+namespace PowerForDis
+{
+    
+}
---
+++ power_training_app/Models_dis/PowerForDisUser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Configuration.Assemblies;
+using System.Dynamic;
+using System.IO.Pipes;
+using System.Security.Cryptography.X509Certificates;
+namespace PowerForDis
+{
+    class PowerFordisUser
+    {
+        protected string nameU;
+        protected int age;
+        protected double wight;
+        protected double hight;
+
+        public PowerFordisUser(string nameU, int age, double wight, double hight)
+        {
+            this.nameU = nameU;
+            this.age = age;
+            this.wight = wight;
+            this.hight = hight;
+        }
+        public string NameU
+        {
+            get {return nameU;}
+            set {nameU = value;}
+        }
+        public int Age
+        {
+            get {return age;}
+            set {age = value;}
+        }
+        public double Wight
+        {
+            get {return wight;}
+            set {wight = value;}
+        }
+        public double Hight
+        {
+            get {return hight;}
+            set {hight = value;}
+        }
+        public virtual string workoutsummery()
+        {
+            return $"the {nameU} finished his workout";
+        }
+        public virtual bool workoutsafty()
+        {
+            return true;
+        }
+        public virtual string workoutinstraction()
+        {
+            return $"Hi {nameU} dont forget to do this easy and slow";
+        }
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine($"{nameU} {age} {wight} {hight}");
+        }
+    }
+}
